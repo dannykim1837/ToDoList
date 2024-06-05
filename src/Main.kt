7a83475d845159deb13346776b7f6cd8fe526315
dannykim1837 <eoghks1837@gmail.com>
2024-06-04 21:22:41 -0600

    push

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,80 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import javax.swing.*
+import java.awt.*
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class ToDoListApp : JFrame() {
+    private val todoListModel = DefaultListModel<String>()
+    private val todoList = JList(todoListModel)
+
+    init {
+        createUI()
+    }
+
+    private fun createUI() {
+        title = "To-Do List App"
+        setSize(400, 300)
+        defaultCloseOperation = EXIT_ON_CLOSE
+        layout = CardLayout()
+
+        // Start screen
+        val startPanel = JPanel()
+        startPanel.layout = BoxLayout(startPanel, BoxLayout.Y_AXIS)
+        val startLabel = JLabel("Welcome to the To-Do List App")
+        startLabel.alignmentX = Component.CENTER_ALIGNMENT
+        val startButton = JButton("Start")
+        startButton.alignmentX = Component.CENTER_ALIGNMENT
+        startPanel.add(Box.createVerticalGlue())
+        startPanel.add(startLabel)
+        startPanel.add(Box.createVerticalStrut(20))
+        startPanel.add(startButton)
+        startPanel.add(Box.createVerticalGlue())
+        add(startPanel, "start")
+
+        // To-Do List screen
+        val mainPanel = JPanel()
+        mainPanel.layout = BorderLayout()
+
+        val inputPanel = JPanel()
+        inputPanel.layout = FlowLayout()
+        val inputField = JTextField(20)
+        val addButton = JButton("Add Task")
+        inputPanel.add(inputField)
+        inputPanel.add(addButton)
+        mainPanel.add(inputPanel, BorderLayout.NORTH)
+
+        val scrollPane = JScrollPane(todoList)
+        mainPanel.add(scrollPane, BorderLayout.CENTER)
+
+        val removeButton = JButton("Remove Selected Task")
+        mainPanel.add(removeButton, BorderLayout.SOUTH)
+        add(mainPanel, "main")
+
+        val cl = contentPane.layout as CardLayout
+        startButton.addActionListener {
+            cl.show(contentPane, "main")
+        }
+
+        addButton.addActionListener {
+            val task = inputField.text
+            if (task.isNotEmpty()) {
+                todoListModel.addElement(task)
+                inputField.text = ""
+            }
+        }
+
+        removeButton.addActionListener {
+            val selectedIndex = todoList.selectedIndex
+            if (selectedIndex != -1) {
+                todoListModel.remove(selectedIndex)
+            }
+        }
+    }
+}
+
+fun main() {
+    SwingUtilities.invokeLater {
+        val app = ToDoListApp()
+        app.isVisible = true
     }
 }
